72d5ebf3e3aeac2b4d1b27bfbc3efd978aebbbe0
Harshit-Chaudhry <Chaudhary.Harshit1203@gmail.com>
2025-03-30 18:10:20 +0530

    change the method of defining the model path in to get model in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import pickle
 import pandas as pd
+import os
 #from data_model import Medical
 from src.data_model import Medical
 
@@ -10,9 +11,9 @@ app = FastAPI(
     description = "Predicting your Medical Cost "
 )
 
-with open(r"D:/projects/Medical-cost-predictor/model.pkl", "rb") as f:
+model_path = os.path.join(os.path.dirname(__file__), "..", "model.pkl")
+with open(model_path, "rb") as f:
     model = pickle.load(f)
-
 
 @app.get("/")
 def index():
