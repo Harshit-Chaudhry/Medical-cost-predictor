92c8ba10df0a0d24b739477ca6b62f2699a57d21
Harshit-Chaudhry <Chaudhary.Harshit1203@gmail.com>
2025-03-30 18:30:50 +0530

    Added model.pkl

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 import pickle
 import pandas as pd
 import os
-#from data_model import Medical
 from src.data_model import Medical
 
 
@@ -12,6 +11,7 @@ app = FastAPI(
 )
 
 model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
+
 with open(model_path, "rb") as f:
     model = pickle.load(f)
 
